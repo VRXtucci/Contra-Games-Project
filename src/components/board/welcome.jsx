f4ee1f5d6d64c320f41b66b15aa22878a0ed06f3
VRXtucci <tucci.jose023@gmail.com>
2024-03-04 16:36:14 -0400

    Modificando welcome agregandole estilos Tailwind

--- src/components/board/welcome.jsx
+++ src/components/board/welcome.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import Gif from '../../assets/contra.gif'
 import './welcome.css'
 
 function Welcome() {
     return <>
-        <main class="container">
-
-            <h1 class='saludo'>Welcome to Contra Game</h1>
-            <button class='saludos'>Click Here for play</button>
+        <main>
+            <img src={Gif} alt="Fondo" className='absolute h-screen w-screen' />
+            <div className='relative text-white'>
+                <h1 className='flex py-5 justify-center items-center font-black text-white text-2xl'>Welcome to Contra Game</h1>
+                <div className='flex justify-center items-center content-center h-screen w-screen'>
+                    <button className='font-black text-white text-xl'>Click Here for play</button>
+                </div>
+            </div>
         </main>
-        
-    
     </>
 }
 
