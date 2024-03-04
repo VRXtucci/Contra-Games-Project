98e9d17236c4bae8def40d6d585dac4cc5f9589c
dracosblood <djtb1994iut@gmail.com>
2024-03-04 11:53:24 -0400

    Creater welcomen page for Darwin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Welcome from './components/board/welcome';
 import './index.css';
 
 
 function App() {
   return (
     <div className="bg-gray-800 h-screen">
+      <div>
+      <Welcome></Welcome>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/board/welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './welcome.css'
+
+function Welcome() {
+    return <>
+        <main class="container">
+
+            <h1 class='saludo'>Welcome to Contra Game</h1>
+            <button class='saludos'>Click Here for play</button>
+        </main>
+        
+    
+    </>
+}
+
+export default Welcome
+
